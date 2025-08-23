7ec8b839a44be549afaaf4d722d3a0212f03306a
Zac Orndorff <zac@orndorff.dev>
2025-08-23 13:38:40 -0500

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
 
     imageService: "cloudflare"
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
